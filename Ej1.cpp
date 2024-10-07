8387bc25104dd13a59a6475bbb3b774b6978ca6e
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-10-07 15:47:29 -0300

    solo puse el enunciado

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
-using namespace std;
+/*Simulación de un pequeño diccionario. Utilizaremos el HashMap para almacenar pares de
+palabras (clave) y sus significados (valor).
+El usuario podrá:
+● Agregar nuevas palabras y sus significados.
+● Buscar el significado de una palabra.
+● Eliminar una palabra del diccionario.
+● Ver todas las palabras en el diccionario.*/
 
-int main() {
-
-    cout << "Ejercicio N° 1" << endl;
-
-    return 0;
-}
